837d231ad4f0812deb659d02abeb23b60f137d33
Vu Quoc Hung <hungvu@1-hungvu-m1.local>
2024-04-17 15:08:18 +0700

    quick config

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,28 @@
+return {
+  "catppuccin",
+  opts = {
+    flavour = "macchiato",
+    transparent_background = false,
+    term_colors = false,
+    styles = {
+      comments = { "italic" },
+      conditionals = { "italic" },
+      loops = { "italic" },
+      functions = { "bold" },
+      keywords = { "italic" },
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = { "italic" },
+      operators = { "italic" },
+      -- variables = { "italic"},
+    },
+    dim_inactive = {
+      enabled = false,
+      shade = "dark",
+      percentage = 0.15,
+    },
+  },
+}
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -12,6 +10,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
+        "pyright",
         -- add more arguments for adding more language servers
       })
     end,
@@ -26,6 +25,7 @@ return {
         "prettier",
         "stylua",
         -- add more arguments for adding more null-ls sources
+        "sourcery",
       })
     end,
   },
